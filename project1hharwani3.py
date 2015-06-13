3097da958f4e469b2e200b490f6a01818f65e8e3
Harsh Rajesh Harwani <harwaniharsh@gmail.com>
2015-06-13 12:09:41 -0400

    writing functions done, need to remove special characters

--- project1hharwani3.py
+++ project1hharwani3.py
@@ -1,4 +1,7 @@
+#@author hharwani
+
 import operator
+
 ##opening the input file and reading it
 ##returns the list of lines from the file which is read.
 def openFile(filename):
@@ -56,30 +59,7 @@ def getSortedList(wordDict):
     sortedWordList=sorted(wordDict,key=wordDict.get,reverse=True)
     return sortedWordList
 
-def writeTop10intoFile(outputFilename,sortedWordList,wordDict):
-    fileObject=open(outputFilename,'a')
-    fileObject.write("\n")
-    fileObject.write("The top 10 words are as follows:")
-    count=0
-    for i in range(len(sortedWordList)):
-        if count>=10:
-            break;
-        fileObject.write("\n")
-        fileObject.write(sortedWordList[i])
-        fileObject.write("\t")
-        fileObject.write(str(wordDict.get(sortedWordList[i])))
-        count+=1
-    fileObject.close()
-    
-def writeTotalWordsIntoFile(outputFilename,wordList):
-    fileObject=open(outputFilename,'w')
-    fileObject.write("The total no of words in the file are:")
-    fileObject.write("\n")
-    fileObject.write(str(len(wordList)))
-    fileObject.write("\n")
-    fileObject.close()
-
-
+#builds the character dictionary with char as key and their occurences as value
 def buildCharDict(processedWordList):
     charDict={}
     for word in processedWordList:
@@ -93,6 +73,7 @@ def buildCharDict(processedWordList):
                 charDict.update({char:1})
     return charDict
 
+#counts the total number of characters in the file
 def getTotalNoChar(charDict):
     values=charDict.values()
     sum=0;
@@ -100,6 +81,7 @@ def getTotalNoChar(charDict):
         sum+=value
     return sum
 
+#calculates the frequency as percentage of various characters in the file.
 def getFrequencyPerc(sortedCharList,charDict,totalNoOfChar):
     charFrequencyDict={}
     for char in sortedCharList:
@@ -109,6 +91,53 @@ def getFrequencyPerc(sortedCharList,charDict,totalNoOfChar):
     return charFrequencyDict
     
 
+
+##writing functions for writing various stastics in the file
+def writeTop10intoFile(outputFilename,sortedWordList,wordDict):
+    fileObject=open(outputFilename,'a')
+    fileObject.write("\n")
+    fileObject.write("The top 10 words are as follows:")
+    fileObject.write("\n")
+    count=0
+    for i in range(len(sortedWordList)):
+        if count>=10:
+            break;
+        fileObject.write(sortedWordList[i])
+        fileObject.write("\t")
+        fileObject.write(str(wordDict.get(sortedWordList[i])))
+        fileObject.write("\n")
+        count+=1
+    fileObject.close()
+    
+def writeTotalWordsIntoFile(outputFilename,wordList):
+    fileObject=open(outputFilename,'a')
+    fileObject.write("\n")
+    fileObject.write("The total no of words in the file are:")
+    fileObject.write("\n")
+    fileObject.write(str(len(wordList)))
+    fileObject.write("\n")
+    fileObject.close()
+
+def writeTotalCharIntoFile(outputFilename,totalNoOfChar):
+    fileObject=open(outputFilename,'a')
+    fileObject.write("The total no of non-whitespace characters in the file are:")
+    fileObject.write("\n")
+    fileObject.write(str(totalNoOfChar))
+    fileObject.write("\n")
+    fileObject.close()
+
+def writeCharFrequency(outputFilename,charFrequencyDict):
+    fileObject=open(outputFilename,'w')
+    fileObject.write("The characters and their frequency are as follows:")
+    fileObject.write("\n")
+    for item in charFrequencyDict.keys():
+        fileObject.write(item)
+        fileObject.write("\t")
+        fileObject.write(str(charFrequencyDict.get(item)))
+        fileObject.write("\n")
+
+##
+        
 ##Declaration area
 flag=True
 
@@ -129,17 +158,11 @@ while flag:
         #processing the word list, removing trailing and leading whutespaces and lowercasing
         processedWordList=getProcessedWordsList(wordList)
         
-        #writing total number of words in the file.
-        writeTotalWordsIntoFile(outputFilename,wordList)
-
         #building the word dictionary
         wordDict=buildwordDict(processedWordList)
 
         #sorting the dictionary to get the top frequency words
         sortedWordList=getSortedList(wordDict)
-
-        #writing the top 10 words in the file
-        writeTop10intoFile(outputFilename,sortedWordList,wordDict)
 
         #building the character dictionary
         charDict=buildCharDict(processedWordList)
@@ -149,14 +172,22 @@ while flag:
 
         #getting the total no of non-white space characters.
         totalNoOfChar=getTotalNoChar(charDict)
-        print(totalNoOfChar)
 
         #calculating the frequency as percentage for each of the characters
         charFrequencyDict=getFrequencyPerc(sortedCharList,charDict,totalNoOfChar)
+        
+        #writing the frequency of different characters in the file
+        writeCharFrequency(outputFilename,charFrequencyDict)
 
-        for item in charFrequencyDict.keys():
-                print(item,str(charFrequencyDict.get(item)))
-       
+        #writing the top 10 words in the file
+        writeTop10intoFile(outputFilename,sortedWordList,wordDict)
+
+        #writing total number of words in the file.
+        writeTotalWordsIntoFile(outputFilename,wordList)
+
+        #writing the total no of non-whitespace characters in the file.
+        writeTotalCharIntoFile(outputFilename,totalNoOfChar)
+           
     else:
         print("Please enter a valid filename")
     
